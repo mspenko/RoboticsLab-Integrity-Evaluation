ab2f356729a52e7a71e492ee913b71c8fe4f0d9c
Yihe Chen <chenyh570@yahoo.com>
2019-06-25 22:17:58 -0500

    modified:   SLAM/main.py 	modified:   utils/class/CountersClass.py 	modified:   utils/functions/__pycache__/R_NB_rot.cpython-36.pyc 	modified:   utils/functions/__pycache__/body2nav_3D.cpython-36.pyc 	modified:   utils/functions/__pycache__/ecef2lla.cpython-36.pyc

--- SLAM/main.py
+++ SLAM/main.py
@@ -4,19 +4,20 @@ import sys
 sys.path.insert(0,'../utils/functions/')
 sys.path.insert(0,'../utils/class/')
 import ParametersClass
+import EstimatorClassSlam
 import CountersClass
 import LidarClass
 #import DataClass
 import GPSClass
 import LidarClass
 import IMUClass
-
+import DataClass
 
 # create objects
 params= ParametersClass.ParametersClass('slam');
-#gps= GPSClass.GPSClass(params.num_epochs_static * params.dt_imu, params);
-lidar= LidarClass.LidarClass(params,70478) #gps.timeInit);
-imu= IMUClass.IMUClass(params, 70478);
+gps= GPSClass.GPSClass(params.num_epochs_static * params.dt_imu, params);
+lidar= LidarClass.LidarClass(params,gps.timeInit);
+#imu= IMUClass.IMUClass(params, 70478);
 #estimator= EstimatorClassSlam(imu.inc_msmt(1:3, params.num_epochs_static), params);
-#data_obj= DataClass(imu.num_readings, gps.num_readings, params);
-#counters= CountersClass(gps, lidar, params);
+data_obj= DataClass.DataClass(302, gps.num_readings, params);#imu.num_readings
+counters= CountersClass.CountersClass(gps, lidar, params);
