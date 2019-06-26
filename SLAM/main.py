e93b9b1ca60021adf1d4eab6959b23e5945541a7
Yihe Chen <chenyh570@yahoo.com>
2019-06-26 00:43:38 -0500

    modified:   SLAM/main.py 	modified:   utils/class/IMUClass.py

--- SLAM/main.py
+++ SLAM/main.py
@@ -17,7 +17,7 @@ import DataClass
 params= ParametersClass.ParametersClass('slam');
 gps= GPSClass.GPSClass(params.num_epochs_static * params.dt_imu, params);
 lidar= LidarClass.LidarClass(params,gps.timeInit);
-#imu= IMUClass.IMUClass(params, 70478);
+imu= IMUClass.IMUClass(params, gps.timeInit);
 #estimator= EstimatorClassSlam(imu.inc_msmt(1:3, params.num_epochs_static), params);
 data_obj= DataClass.DataClass(302, gps.num_readings, params);#imu.num_readings
 counters= CountersClass.CountersClass(gps, lidar, params);
