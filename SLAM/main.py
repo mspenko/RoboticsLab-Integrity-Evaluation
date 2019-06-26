03b7bea4c095b247d27abfe0297aa5cab7ec5fce
Osama <oabdulhafez@hawk.iit.edu>
2019-06-26 13:23:42 -0500

    Merge with master

--- SLAM/main.py
+++ SLAM/main.py
@@ -14,9 +14,9 @@ import IMUClass
 
 # create objects
 params= ParametersClass.ParametersClass('slam');
-#gps= GPSClass.GPSClass(params.num_epochs_static * params.dt_imu, params);
-lidar= LidarClass.LidarClass(params,70478) #gps.timeInit);
-imu= IMUClass.IMUClass(params, 70478);
+gps= GPSClass.GPSClass(params.num_epochs_static * params.dt_imu, params);
+lidar= LidarClass.LidarClass(params,gps.timeInit);
+#imu= IMUClass.IMUClass(params, 70478);
 #estimator= EstimatorClassSlam(imu.inc_msmt(1:3, params.num_epochs_static), params);
 #data_obj= DataClass(imu.num_readings, gps.num_readings, params);
-#counters= CountersClass(gps, lidar, params);
+counters= CountersClass.CountersClass(gps, lidar, params);
