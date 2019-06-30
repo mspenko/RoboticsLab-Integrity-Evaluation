724c876d41e5eb35c6595dc62c0ad27712c53127
Yihe Chen <chenyh570@yahoo.com>
2019-06-30 13:00:39 -0500

    modified:   utils/class/FGDataInputClass.py

--- utils/class/FGDataInputClass.py
+++ utils/class/FGDataInputClass.py
@@ -21,9 +21,9 @@ class FGDataInputClass:
           self.associations= [None]*num_readings;
 
       def delete_fields_corresponding_to_static_epochs(self, lidar):
-          self.lidar = np.delete(self.lidar,np.s_[1:lidar.index_of_last_static_lidar_epoch],axis =0)
-          self.imu = np.delete(self.imu,np.s_[1:lidar.index_of_last_static_lidar_epoch],axis =0)
-          self.gps_msmt = np.delete(self.gps_msmt,np.s_[1:lidar.index_of_last_static_lidar_epoch],axis =0)
-          self.gps_R = np.delete(self.gps_R,np.s_[1:lidar.index_of_last_static_lidar_epoch],axis =0)
-          self.pose = np.delete(self.pose,np.s_[1:lidar.index_of_last_static_lidar_epoch],axis =0)
-          self.associations = np.delete(self.associations,np.s_[1:lidar.index_of_last_static_lidar_epoch],axis =0)
+          self.lidar = np.delete(self.lidar,np.s_[0:lidar.index_of_last_static_lidar_epoch+1],axis =0)
+          self.imu = np.delete(self.imu,np.s_[0:lidar.index_of_last_static_lidar_epoch+1],axis =0)
+          self.gps_msmt = np.delete(self.gps_msmt,np.s_[0:lidar.index_of_last_static_lidar_epoch+1],axis =0)
+          self.gps_R = np.delete(self.gps_R,np.s_[0:lidar.index_of_last_static_lidar_epoch+1],axis =0)
+          self.pose = np.delete(self.pose,np.s_[0:lidar.index_of_last_static_lidar_epoch+1],axis =0)
+          self.associations = np.delete(self.associations,np.s_[0:lidar.index_of_last_static_lidar_epoch+1],axis =0)
